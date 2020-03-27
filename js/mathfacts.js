a80bbbb8da0468e47306d56049078a379a4a0f9a
Nathaniel Mitchell <nathaniel.p.mitchell@intel.com>
2020-03-27 11:40:06 -0700

    "Fixed settings per enhancement issue"

--- js/mathfacts.js
+++ js/mathfacts.js
@@ -6,35 +6,38 @@ var progbox = document.getElementById("progbox");
 var prog = document.getElementById("prog");
 var addcheck = document.getElementById("addcheck");
 var subcheck = document.getElementById("subcheck");
-var multicheck = document.getElementById("multicheck")
-var divcheck = document.getElementById("divcheck")
+var multicheck = document.getElementById("multicheck");
+var divcheck = document.getElementById("divcheck");
 var allownegcheck = document.getElementById("allownegcheck");
 var resetbutton = document.getElementById("resetbutton");
 var maxnumbox = document.getElementById("maxnumbox");
 var settingsdiv = document.getElementById("settingsdiv");
+var settingshide = document.getElementById("settingshide");
 var hsdiv = document.getElementById("hsdiv");
-maxnumbox.value = 12;
+var qnumbox = document.getElementById("qnumbox");
+
+
 var timeperq = 15 * 1000; //ms
 var refreshinterval = 50; //ms
 var allowneganswers = false; 
-var numberofq = 5;
 var successmessages =  ["You did it!", 
                         "Correct!", 
                         "Nice one!", 
                         "Woo-hoo!"];
 
 function getRandomInt(max) {
-    return Math.round(Math.random() * Math.floor(max));
+	return Math.round(Math.random() * Math.floor(max));
 }
 function getRandomIndex(max) {
-    return Math.ceil(Math.random() * Math.floor(max)) - 1;
+	return Math.ceil(Math.random() * Math.floor(max)) - 1;
 }
 function getRandomSuccess(){
-    return successmessages[getRandomIndex(successmessages.length)];
+	return successmessages[getRandomIndex(successmessages.length)];
 }
 
 var supportedops;
-
+var maxnum;
+var qnum;
 var currtime = timeperq;
 var proginterval;
 var currx;
@@ -47,164 +50,215 @@ var questioncount = 0;
 var hs;
 
 function newQuestion(){
-    clearTimeout(newqtimeout);
-    
-    if(questioncount === numberofq){
-        clearTimeout(newqtimeout);
-        progStop();
-        questionbox.innerHTML = "Game over!";
-        highscoreSet();
-        return;
-    }else{
-        do{
-            currx = getRandomInt(maxnumbox.value);
-            curry = getRandomInt(maxnumbox.value);
-            opindex = getRandomIndex(supportedops.length);
-            //opindex = opindex < 0 ? 0 : opindex; 
-            currop = supportedops[opindex];
-            var questionstr="" + currx + " " + currop + " " + curry;
-            questionbox.innerHTML = questionstr;
-            realanswer = Number(eval(questionstr));
-            if (Number.isNaN(realanswer)){
-                questionbox.innerHTML = "Something went wrong...";
-            }
-        } while(!allowneganswers && realanswer < 0)
-        progReset();
-        progStart();
-        newqtimeout = setTimeout(() => {
-            progStop();
-            answerClear();
-            //scoreSub(1);
-            newQuestion();
-        }, timeperq);
-    }
-    questioncount++;
-    progbox.innerHTML = "Q " + questioncount + " of " + numberofq;
+	clearTimeout(newqtimeout);
+	if(questioncount === qnum){
+		clearTimeout(newqtimeout);
+		progStop();
+		questionbox.innerHTML = "Game over!";
+		highscoreSet();
+		return;
+	}else{
+		do{
+			currx = getRandomInt(maxnum);
+			curry = getRandomInt(maxnum);
+			opindex = getRandomIndex(supportedops.length);
+			//opindex = opindex < 0 ? 0 : opindex; 
+			currop = supportedops[opindex];
+			var questionstr="" + currx + " " + currop + " " + curry;
+			questionbox.innerHTML = questionstr;
+			realanswer = Number(eval(questionstr));
+			if (Number.isNaN(realanswer)){
+				questionbox.innerHTML = "Something went wrong...";
+			}
+		} while(!allowneganswers && realanswer < 0)
+		progReset();
+		progStart();
+		newqtimeout = setTimeout(() => {
+			progStop();
+			answerClear();
+			//scoreSub(1);
+			newQuestion();
+		}, timeperq);
+	}
+	questioncount++;
+	progbox.innerHTML = "Q " + questioncount + " of " + qnum;
 }
 
 function highscoreGet(){
-    hs = localStorage.getItem("highscore");
-    if(hs === null){
-        localStorage.setItem("highscore", 0);
-        hs=0;
-    }
-    return Number(hs);
+	hs = localStorage.getItem("highscore");
+	if(hs === null){
+		localStorage.setItem("highscore", 0);
+		hs=0;
+	}
+	return Number(hs);
 }
 
 function highscoreSet(){
-    var currscore = Number(scorebox.innerHTML);
-    var currhs = highscoreGet();
-    if (currscore > currhs){
-        currhs = currscore;
-	showHsDiv();	
-        //alert("New high score! Way to go!");
-        localStorage.setItem("highscore", currhs);
-        
-    }
-    hsbox.innerHTML = currhs
-    
+	var currscore = Number(scorebox.innerHTML);
+	var currhs = highscoreGet();
+	if (currscore > currhs){
+		currhs = currscore;
+	showHsDiv();
+		//alert("New high score! Way to go!");
+		localStorage.setItem("highscore", currhs);
+		
+	}
+	hsbox.innerHTML = currhs
 }
 
 function showHsDiv(){
 	hsdiv.style.display = "";
 	setTimeout(() => {
 		hsdiv.style.display = "none";
-        }, 2.5 * 1000);
-
-
-
+		}, 2.5 * 1000);
 }
 
 function highscoreReset(){
-    localStorage.setItem("highscore", 0);
-    hsbox.innerHTML = 0
+	localStorage.setItem("highscore", 0);
+	hsbox.innerHTML = 0
 }
 
 function scoreReset(){
-    scorebox.innerHTML = "0";
+	scorebox.innerHTML = "0";
 }
 
 function scoreAdd(amount){
-    scorebox.innerHTML = (Number(scorebox.innerHTML) + amount);
+	scorebox.innerHTML = (Number(scorebox.innerHTML) + amount);
 }
 
 function scoreSub(amount){
-    scorebox.innerHTML = (Number(scorebox.innerHTML) - amount);
+	scorebox.innerHTML = (Number(scorebox.innerHTML) - amount);
 }
 
 function scoreGet(){
-    return Number(scorebox.innerHTML);
+	return Number(scorebox.innerHTML);
 }
 
 function progStop(){
-    if (proginterval != null)
-        clearInterval(proginterval);
+	if (proginterval != null)
+		clearInterval(proginterval);
 }
 
 function progStart(){
-    proginterval = setInterval(() => {
-        currtime -= refreshinterval;
-        prog.value = (currtime/timeperq * 100);
-    }, refreshinterval)
+	proginterval = setInterval(() => {
+		currtime -= refreshinterval;
+		prog.value = (currtime/timeperq * 100);
+	}, refreshinterval)
 }
 
 function progReset(){
-    currtime = timeperq;
-    prog.value = 100;
+	currtime = timeperq;
+	prog.value = 100;
 }
 
 function answerClear(){
-    answerbox.value = "";
+	answerbox.value = "";
 }
 
 function checkAnswer(){    
-    inanswer = answerbox.value == "" ? Number.NaN : Number(answerbox.value);
-    //console.log("inanswer: " + inanswer + " realanswer: " + realanswer);
-    if (realanswer === inanswer){
-        clearTimeout(newqtimeout);
-        progStop();
-        scoreAdd(Math.round(Math.pow(1.1,(currtime/timeperq * 100))));
-        questionbox.innerHTML = getRandomSuccess();
-        newqtimeout = setTimeout(() => {
-            newQuestion()
-        }, 2 * 1000);
-        answerClear();
-    }
-        
+	inanswer = answerbox.value == "" ? Number.NaN : Number(answerbox.value);
+	//console.log("inanswer: " + inanswer + " realanswer: " + realanswer);
+	if (realanswer === inanswer){
+		clearTimeout(newqtimeout);
+		progStop();
+		scoreAdd(Math.round(Math.pow(1.1,(currtime/timeperq * 100))));
+		questionbox.innerHTML = getRandomSuccess();
+		newqtimeout = setTimeout(() => {
+			newQuestion()
+		}, 2 * 1000);
+		answerClear();
+	}
 }
 
 function updateSupportedOps(){
-    supportedops = [];
-    if (subcheck.checked)
-        supportedops.push("-");
-    if (addcheck.checked)
-        supportedops.push("+");
-    if (divcheck.checked)
-        supportedops.push("/");
-    if (multicheck.checked)
-        supportedops.push("*");
-    allowneganswers = allownegcheck.checked;
-    answerbox.focus();
+	supportedops = [];
+	if (subcheck.checked)
+		supportedops.push("-");
+	if (addcheck.checked)
+		supportedops.push("+");
+	if (divcheck.checked)
+		supportedops.push("/");
+	if (multicheck.checked)
+		supportedops.push("*");
+	allowneganswers = allownegcheck.checked;
+}
+
+function settingsChangedCallback(event){
+	localStorage.setItem("setting"+event.target.value, String(event.target.checked));
+	answerbox.focus();
 }
 
 function newGame(){
-    scoreReset();
-    questioncount = 0;
-    clearTimeout(newqtimeout);
-    progStop();
-    updateSupportedOps();
-    newQuestion();
-    answerbox.focus();
+	maxnum = Number(maxnumbox.value);
+	qnum = Number(qnumbox.value);
+	scoreReset();
+	questioncount = 0;
+	clearTimeout(newqtimeout);
+	progStop();
+	updateSupportedOps();
+	newQuestion();
+	answerbox.focus();
 }
 
-answerbox.addEventListener("keyup", checkAnswer);
+function showSettings(){
+	if (settingshide.innerHTML == "˄"){
+		settingshide.innerHTML = "˅";
+		settingsdiv.style.display = "none";
+	}else{
+		settingshide.innerHTML = "˄";
+		settingsdiv.style.display = "";
+	}
+}
 
-subcheck.addEventListener("click", updateSupportedOps);
-addcheck.addEventListener("click", updateSupportedOps);
-divcheck.addEventListener("click", updateSupportedOps);
-multicheck.addEventListener("click", updateSupportedOps);
-allownegcheck.addEventListener("click", updateSupportedOps);
+function loadSettings(){
+	var allowsub = localStorage.getItem("setting-");
+	if (allowsub != null){
+		subcheck.checked = allowsub == "true";
+	}
+	var allowadd = localStorage.getItem("setting+");
+	if (allowadd != null){
+		addcheck.checked = allowadd == "true";
+	}
+	var allowmulti = localStorage.getItem("setting*");
+	if (allowmulti != null){
+		multicheck.checked = allowmulti == "true";
+	}
+	var allowdivi = localStorage.getItem("setting/");
+	if (allowdivi != null){
+		divcheck.checked = allowdivi == "true";
+	}
+	var allowneg = localStorage.getItem("settingallowneg");
+	if (allowneg != null){
+		allownegcheck.checked = allowneg == "true";
+	}
+	var maxnumber = localStorage.getItem("settingmaxnumbox");
+	if (maxnumber != null){
+		maxnumbox.value = Number(maxnumber);
+	}
+	var qnumber = localStorage.getItem("settingqnumbox");
+	if (qnumber != null){
+		qnumbox.value = Number(qnumber);
+	}
+}
+
+function numChangeedCallback(event){
+	localStorage.setItem("setting"+event.target.id, String(event.target.value));
+	answerbox.focus();
+}
+
+loadSettings();
+
+answerbox.addEventListener("keyup", checkAnswer);
+subcheck.addEventListener("click", settingsChangedCallback);
+addcheck.addEventListener("click", settingsChangedCallback);
+divcheck.addEventListener("click", settingsChangedCallback);
+multicheck.addEventListener("click", settingsChangedCallback);
+allownegcheck.addEventListener("click", settingsChangedCallback);
+maxnumbox.addEventListener("change", numChangeedCallback);
+qnumbox.addEventListener("change", numChangeedCallback);
 resetbutton.addEventListener("click", newGame);
+settingshide.addEventListener("click", showSettings);
+
 
 highscoreSet(0);
 
